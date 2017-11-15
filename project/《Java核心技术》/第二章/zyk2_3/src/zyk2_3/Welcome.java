36f67697135d1cd90fd738e814cf2bfd9b2f3862
youkuan.zeng <365085442@qq.com>
2017-11-15 21:39:52 +0800

    完成第二章2-3作业

---
+++ project/《Java核心技术》/第二章/zyk2_3/src/zyk2_3/Welcome.java
@@ -0,0 +1,12 @@
+package zyk2_3;
+
+public class Welcome {
+	public static void main(String[] ar){
+		String greeting = "Welcome to Croe Java!";
+		System.out.println(greeting);
+		for (int i = 0; i < greeting.length(); i++)
+			System.out.print("=");
+		System.out.println();
+	}
+
+}
