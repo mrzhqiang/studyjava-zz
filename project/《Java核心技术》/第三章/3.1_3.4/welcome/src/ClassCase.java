9a61a48daa56a9c8c3e322081fee4f6f799d16c2
qiang.zhang <287431404@qq.com>
2017-11-19 01:09:13 +0800

    上传一起看第三章3.1-3.4时建立的工程

---
+++ project/《Java核心技术》/第三章/3.1_3.4/welcome/src/ClassCase.java
@@ -0,0 +1,51 @@
+
+
+public class ClassCase {
+	public static final double YUANZHOULUPAI = 3.1415926;
+	public static void main(String[] aaaaa) {
+		// 如果使用两个斜杠，就代表这一行是注释文字，编译器就会跳过这部分
+		// 啊大时代
+		
+/*
+ 这个是多行注释
+ 回车生效
+ */
+//		System.out.println(YUANZHOULUPAI);
+		
+		// long型数值需要加后缀L或l
+//		long date = 21474836471L;
+		
+		// 1111 这是一个二进制数15 》》》 0 1 2的0次方=1,2的1次方=2
+		// 0xA=10 0xB=11 0xC=12 ... 0xF=15
+		// 1111 == 0xF
+		// 0x1F = 31
+		// 1111 1111 1111 1111
+//		double a = 3.141592611;
+//		float a = 3.141592611F;
+//		System.out.println(a);
+
+//		System.out.println(0/0.0);
+//		System.out.println(0.00/0.0);
+//		System.out.println(0.0==0.0);
+//		System.out.println(0.00/0.0==0/0.0);
+		
+
+//		System.out.println(2.0 - 1.1);
+
+		// 这样调用会造成死循环
+//		ClassCase.main(aaaaa);
+		
+//		ClassCase mCase=null,aCase;
+//		mCase.show();
+		
+//		final int a = 1;
+//		a = 2;
+		
+		
+	}
+	
+	public void show() {
+		System.out.println(2.0 - 1.1);
+	}
+
+}
