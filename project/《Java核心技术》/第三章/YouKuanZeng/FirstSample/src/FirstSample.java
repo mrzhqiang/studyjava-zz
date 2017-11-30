ee8dbe6567c134fea5d7febb7eb5ad98826d26b2
YouKuanZeng <365085442@qq.com>
2017-11-30 21:34:47 +0800

    CoreJava第三章程序清单1到5

---
+++ project/《Java核心技术》/第三章/YouKuanZeng/FirstSample/src/FirstSample.java
@@ -0,0 +1,10 @@
+/*
+ * This is the first sample program in Core Java Chapter 3
+ */
+public class FirstSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We will not use'Hello, World!'");
+	}
+}
