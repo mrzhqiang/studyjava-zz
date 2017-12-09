e572e1e3091ed89390384954b52d9e592752e596
YouKuanZeng <365085442@qq.com>
2017-12-09 19:16:34 +0800

    java第7章部分范例和练习

---
+++ project/《java从入门到精通》/zyk_java7practice1/src/zyk_java7practice1/Practice1.java
@@ -0,0 +1,11 @@
+package zyk_java7practice1;
+
+public class Practice1 {
+	private String name;
+	private void setName(String zhazha) {
+		this.name = zhazha;
+	}
+	public String getName() {
+		return name;
+	}
+}
